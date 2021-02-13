7384a0bd5b1d887d4842beefced1ace763fc626c
Iain Mackay <me@neirfeno.com>
2021-02-13 15:12:27 +0000

    Added basic spring boot configuration from start.spring.io

---
+++ src/main/java/com/neirfeno/munrolibrarychallenge/MunroLibraryChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.neirfeno.munrolibrarychallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunroLibraryChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MunroLibraryChallengeApplication.class, args);
+    }
+
+}
