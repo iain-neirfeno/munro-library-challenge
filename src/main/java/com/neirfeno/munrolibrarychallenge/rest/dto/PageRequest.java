b1b9d962cd27490ee2596d7891425c7731654778
Iain Mackay <me@neirfeno.com>
2021-02-16 20:43:00 +0000

    add conversion of page parameters to Pageable object

--- src/main/java/com/neirfeno/munrolibrarychallenge/rest/dto/PageRequest.java
+++ src/main/java/com/neirfeno/munrolibrarychallenge/rest/dto/PageRequest.java
@@ -1,11 +1,21 @@
 package com.neirfeno.munrolibrarychallenge.rest.dto;
 
+import com.neirfeno.munrolibrarychallenge.repository.UnpagedRequest;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 @Getter
 @Setter
 public class PageRequest {
     private Integer page;
     private Integer limit;
+
+    public Pageable asPageable(Sort sort){
+        if (page == null || limit == null)
+            return new UnpagedRequest(sort);
+
+        return org.springframework.data.domain.PageRequest.of(page, limit, sort);
+    }
 }
