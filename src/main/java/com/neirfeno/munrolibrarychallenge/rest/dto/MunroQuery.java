804fb4b1a89eed7636504067503bd677c287bf60
Iain Mackay <me@neirfeno.com>
2021-02-16 20:48:53 +0000

    add attribute to validate query heights

--- src/main/java/com/neirfeno/munrolibrarychallenge/rest/dto/MunroQuery.java
+++ src/main/java/com/neirfeno/munrolibrarychallenge/rest/dto/MunroQuery.java
@@ -1,11 +1,13 @@
 package com.neirfeno.munrolibrarychallenge.rest.dto;
 
 import com.neirfeno.munrolibrarychallenge.model.HillCategory;
+import com.neirfeno.munrolibrarychallenge.rest.validator.MinHeightNotLessThanMaxHeight;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@MinHeightNotLessThanMaxHeight(message = "The min height cannot be less than the max height")
 public class MunroQuery {
     private HillCategory category;
     private Integer minHeight;
